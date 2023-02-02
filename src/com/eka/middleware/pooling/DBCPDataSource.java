1aab074ebfd3572707aef8d013f8a4ee80c7a43c
Ekamw <info@ekamw.org>
2023-02-02 12:08:22 +0530

    Refactored

--- src/com/eka/middleware/pooling/DBCPDataSource.java
+++ src/com/eka/middleware/pooling/DBCPDataSource.java
@@ -8,28 +8,33 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 public class DBCPDataSource {
-private static final Map<String,DBCPDataSource> dsMap = new ConcurrentHashMap<>();
-private final BasicDataSource ds;
-    private DBCPDataSource(String url,String user,String password, int pool, int timeOut) {
-    	ds=new BasicDataSource();
-    	//dsMap.put(url, ds);
+    private static final Map<String, DBCPDataSource> dsMap = new ConcurrentHashMap<>();
+    private final BasicDataSource ds;
+
+    private DBCPDataSource(String url, String user, String password, int pool, int timeOut) {
+        ds = new BasicDataSource();
+        //dsMap.put(url, ds);
         ds.setUrl(url);
-        if(user!=null) {
-	        ds.setUsername(user);
-	        ds.setPassword(password);
+        if (user != null) {
+            ds.setUsername(user);
+            ds.setPassword(password);
         }
-        ds.setMinIdle(pool/2);
-        ds.setMaxIdle(pool+1);
-        ds.setMaxOpenPreparedStatements((pool+1)*5);
+        ds.setMinIdle(pool / 2);
+        ds.setMaxIdle(pool + 1);
+        ds.setMaxOpenPreparedStatements((pool + 1) * 5);
         ds.setDefaultQueryTimeout(timeOut);
     }
-    
-    public static Connection getConnection(String name,String url,String user,String password, int pool,int timeOut) throws SQLException {
-    	DBCPDataSource dbcp=dsMap.get(name);
-    	if(dbcp==null || dbcp.ds.getDefaultQueryTimeout()!=timeOut || dbcp.ds.getMaxIdle()!=(pool+1)) {
-    		dbcp=new DBCPDataSource(url, user, password, pool,timeOut);
-    		dsMap.put(name,dbcp);
-    	}
+
+    public static Connection getConnection(String name, String url, String user, String password, int pool, int timeOut) throws SQLException {
+        DBCPDataSource dbcp = dsMap.get(name);
+        if (dbcp == null || dbcp.ds.getDefaultQueryTimeout() != timeOut || dbcp.ds.getMaxIdle() != (pool + 1)) {
+            dbcp = new DBCPDataSource(url, user, password, pool, timeOut);
+            dsMap.put(name, dbcp);
+        }
         return dbcp.ds.getConnection();
+    }
+
+    public static void removeConnection(String name) {
+        dsMap.remove(name);
     }
 }
