ac9522852f0f6adc75afa5cb12e3f21bcbd8c214
Ekamw <info@ekamw.org>
2023-04-10 14:16:25 +0530

    Changes * SL-117 Rename all internal variable

--- src/com/eka/middleware/flow/KeywordResolver.java
+++ src/com/eka/middleware/flow/KeywordResolver.java
@@ -13,23 +13,24 @@ public class KeywordResolver {
      */
     public static String find(String expressionKey, DataPipeline dataPipeline) {
 
-        //TODO change to switch case.
-        if (expressionKey.equals("UUID") || expressionKey.equals("*UUID")) {
-            return UUID.randomUUID().toString();
-        } else if (expressionKey.equals("tenant") || expressionKey.equals("*tenant")) {
-            return dataPipeline.rp.getTenant().getName();
-        } else if (expressionKey.equals("CorrelationId") || expressionKey.equals("*CorrelationId")) {
-            return dataPipeline.getCorrelationId();
-        } else if (expressionKey.equals("ContextLocalPath") || expressionKey.equals("*ContextLocalPath")) {
-            if (Boolean.parseBoolean(System.getProperty("CONTAINER_DEPLOYMENT"))) {
-                return "/eka";
-            } else {
-                return "./";
-            }
-        } else if (expressionKey.equals("PackageConfig")) {
-            return dataPipeline.getMyPackageConfigPath();
-        } else {
-            return "null";
+        switch (expressionKey) {
+            case "*UUID":
+                return UUID.randomUUID().toString();
+            case "*tenant":
+                return dataPipeline.rp.getTenant().getName();
+            case "*CorrelationId":
+                return dataPipeline.getCorrelationId();
+
+            case "*ContextLocalPath":
+                if (Boolean.parseBoolean(System.getProperty("CONTAINER_DEPLOYMENT"))) {
+                    return "/eka";
+                } else {
+                    return "./";
+                }
+            case "PackageConfig":
+                return dataPipeline.getMyPackageConfigPath();
+            default:
+                return "null";
         }
     }
 }
