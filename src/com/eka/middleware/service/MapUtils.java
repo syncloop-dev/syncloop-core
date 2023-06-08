0d0e8ddc40e0fd55e4fc24d9fc32bb4164fbea0f
Ekamw <info@ekamw.org>
2023-06-08 22:03:53 +0530

    Refactored * Fixed ClassCastException if Object is not instance of Map.

--- src/com/eka/middleware/service/MapUtils.java
+++ src/com/eka/middleware/service/MapUtils.java
@@ -24,7 +24,7 @@ public class MapUtils {
 		String[] tokenize = pointer.split("/");
 		for (String key : tokenize) {
 			isNumeric = NumberUtils.isCreatable(key);
-			if (obj != null && !isNumeric)
+			if ((obj instanceof Map) && !isNumeric)
 				map = (Map<String, Object>) obj;
 			else if (isNumeric && (obj instanceof List || obj instanceof ArrayList))
 				arrayList = (List<Object>) obj;
