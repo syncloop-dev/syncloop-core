b778c82343092e833532bd2ee3293f74f8a6df50
Ekamw <info@ekamw.org>
2023-10-12 11:30:23 +0530

    Refactored

--- src/com/eka/middleware/scheduling/AppScheduler.java
+++ src/com/eka/middleware/scheduling/AppScheduler.java
@@ -1,5 +1,6 @@
 package com.eka.middleware.scheduling;
 
+import com.eka.middleware.template.Tenant;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,6 +9,12 @@ public class AppScheduler implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         System.out.println("App Scheduler.......");
+
+        run();
+    }
+
+
+    private void run() {
 
     }
 
