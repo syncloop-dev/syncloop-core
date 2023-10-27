818f7eda92b60f2a062d894c092a09d062c7fa34
Ekamw <info@ekamw.org>
2023-10-27 12:51:30 +0530

    Fixed * Connection Pooling

--- src/com/eka/middleware/server/MiddlewareServer.java
+++ src/com/eka/middleware/server/MiddlewareServer.java
@@ -82,8 +82,6 @@ public class MiddlewareServer {
 		try {
 			PropertyManager.initConfig(args);
 			local_IP = PropertyManager.getLocal_IP();
-			Scheduler scheduler = ApplicationSchedulerFactory.initScheduler(null,"default");
-			ApplicationSchedulerFactory.startScheduler(scheduler);
 			String ports[] = ServiceUtils.getServerProperty("middleware.server.http.ports").split(",");
 			String https = ServiceUtils.getServerProperty("middleware.server.https.ports");
 			String keyStoreFilePath = ServiceUtils.getServerProperty("middleware.server.keyStore.jks");
