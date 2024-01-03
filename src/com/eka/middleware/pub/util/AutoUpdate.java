a57429a42bf949f6dc444128495acd0b8aa4f734
divyanshgurtoo <divyanshgurtoo4@gmail.com>
2024-01-03 16:59:06 +0530

    marketPlace/skip_overriding_dependencies

--- src/com/eka/middleware/pub/util/AutoUpdate.java
+++ src/com/eka/middleware/pub/util/AutoUpdate.java
@@ -112,6 +112,104 @@ public class AutoUpdate {
         fis.close();
     }
 
+    public static void unzipForMarketplace(String zipFilePath, String destDir, DataPipeline dp) throws Exception {
+        File dir = new File(destDir);
+        String unZippedFolderPath = null;
+        boolean skipOverride = false; // Flag to skip override if shouldOverride returns false
+
+        // create output directory if it doesn't exist
+        if (!dir.exists()) dir.mkdirs();
+        FileInputStream fis;
+
+        // buffer for read and write data to file
+        fis = new FileInputStream(zipFilePath);
+        ZipInputStream zis = new ZipInputStream(fis);
+        ZipEntry ze = zis.getNextEntry();
+
+        while (ze != null) {
+            String fileName = ze.getName();
+
+            if (unZippedFolderPath == null)
+                unZippedFolderPath = fileName;
+            else {
+                dp.log("Zipped entry " + fileName);
+                fileName = ("#$" + fileName).replace("#$" + unZippedFolderPath, "");
+                File newFile = new File(destDir + File.separator + fileName);
+
+                if (fileName.endsWith("dependency")) {
+                    String packagePropertiesPath = (newFile.getAbsolutePath() + File.separator + "config" + File.separator + "package.properties").replaceAll("\\._", "");
+
+                    if (!shouldOverride(packagePropertiesPath)) {
+                        skipOverride = true;
+                        continue; // Skip override for subsequent files with "dependency" suffix
+                    } else {
+                        // If shouldOverride returns true, reset the flag
+                        skipOverride = false;
+                    }
+                }
+
+                if (!skipOverride) {
+                    if (!fileName.contains("dependency/")) { // Skip the entire "dependency" folder
+                        new File(newFile.getParent()).mkdirs();
+                        byte[] buffer = new byte[1024];
+                        int len = zis.read(buffer);
+
+                        if (len > 0) {
+                            FileOutputStream fos = new FileOutputStream(newFile);
+
+                            while (len > 0) {
+                                fos.write(buffer, 0, len);
+                                len = zis.read(buffer);
+                            }
+
+                            fos.flush();
+                            fos.close();
+                        }
+                    }
+                }
+            }
+
+            // close this ZipEntry
+            zis.closeEntry();
+            ze = zis.getNextEntry();
+        }
+
+        // close last ZipEntry
+        zis.closeEntry();
+        zis.close();
+        fis.close();
+    }
+
+    private static boolean shouldOverride(String packagePropertiesPath) throws IOException {
+        // Check if the package.properties file exists
+        File packagePropertiesFile = new File(packagePropertiesPath);
+
+        if (!packagePropertiesFile.exists() || packagePropertiesFile.isDirectory()) {
+            // If the file doesn't exist or is a directory, allow the override
+            return true;
+        }
+
+        // Load properties only if the file exists
+        Properties properties = new Properties();
+
+        try (FileInputStream input = new FileInputStream(packagePropertiesPath)) {
+            properties.load(input);
+
+            for (String key : properties.stringPropertyNames()) {
+                String value = properties.getProperty(key);
+
+                // If any key has a non-empty value, don't override
+                if (value != null && !value.trim().isEmpty()) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+
     public static void createRestorePoint(String buildName, DataPipeline dataPipeline) throws Exception{
         String packagePath=PropertyManager.getPackagePath(dataPipeline.rp.getTenant());
         String bkpDirPath=packagePath+"builds/backup/";
