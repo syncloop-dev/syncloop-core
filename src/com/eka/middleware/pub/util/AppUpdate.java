2bdb78bf3a40f6b8c3dfc290c945d9d8c371f392
Ekamw <info@ekamw.org>
2024-02-14 19:58:27 +0530

    refactored

--- src/com/eka/middleware/pub/util/AppUpdate.java
+++ src/com/eka/middleware/pub/util/AppUpdate.java
@@ -10,7 +10,11 @@ public class AppUpdate {
     private static final Map<String, Object> UpdatingStatus = Maps.newHashMap();
 
     public static Object getStatus(String uniqueId, DataPipeline dataPipeline) {
-        return UpdatingStatus.get(String.format("%s_%s", dataPipeline.rp.getTenant().getName(), uniqueId));
+        Object status = UpdatingStatus.get(String.format("%s_%s", dataPipeline.rp.getTenant().getName(), uniqueId));
+        if (null == status) {
+            return "COMPLETED_SUCCESS";
+        }
+        return status;
     }
 
     public static void updateStatus(String uniqueId, String status, DataPipeline dataPipeline) {
