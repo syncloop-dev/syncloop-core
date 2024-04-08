a95126a9de445b3e89b4160500cc6bfbab2ca465
DivyanshGurtoo <divyanshgurtoo4@gmail.com>
2024-04-08 11:34:27 +0530

    fixed reliability issues in AuthHandlers.java

--- src/com/eka/middleware/auth/pac4j/AuthHandlers.java
+++ src/com/eka/middleware/auth/pac4j/AuthHandlers.java
@@ -156,7 +156,7 @@ public class AuthHandlers {
 				} else {
 					try {
 						token = ServiceUtils.decrypt(token, tenantName);
-						if(false && !ServiceUtils.isValid(token)) {
+						if(!ServiceUtils.isValid(token)) {
 							throw new Exception("Validation expired.");
 						}
 					} catch (Exception e) {
